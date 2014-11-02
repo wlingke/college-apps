f95d24816aeb484e55c7257cc7329ce13ee737f6
Lingke <wlingke@gmail.com>
2014-11-01 17:36:12 -0700

    minor

--- app.js
+++ app.js
@@ -46,3 +46,5 @@ app.use(function (err, req, res, next) {
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port') + ' in ' + app.get('env') + ' mode.');
 });
+
+console.log('IM RUNNING ********************')
